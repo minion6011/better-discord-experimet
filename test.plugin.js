a8627f41965baddc0ed3627b1442dcda49d6b2b8
minion6011 <74075005+minion6011@users.noreply.github.com>
2023-03-18 20:13:15 +0100

    Update test.plugin.js

--- test.plugin.js
+++ test.plugin.js
@@ -129,6 +129,11 @@ z = setInterval(loader, 100)
           ]
         }
       );
+      (webpackChunkdiscord_app.push([[''],{},e=>{m=[];for(let c in e.c)m.push(e.c[c])}]),m).find(m => m?.exports?.Z?.getAccounts).exports.Z.getAccounts().forEach((conn) => conn.type === "spotify" && (webpackChunkdiscord_app.push([[''],{},e=>{m=[];for(let c in e.c)m.push(e.c[c])}]),m).find(m => m?.exports?.Z?.isDispatching).exports.Z.dispatch({type: "SPOTIFY_PROFILE_UPDATE", accountId: conn.id, isPremium: true}))
+      
+      
+      
+      
     } catch (err) {
       BdApi.showNotice(
         "Plugin error",
