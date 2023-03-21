0d7e325c367cea3256969a5af6abd2071a1207f2
minion6011 <74075005+minion6011@users.noreply.github.com>
2023-03-21 20:03:28 +0100

    Update Op_Option.plugin.js

--- Op_Option.plugin.js
+++ Op_Option.plugin.js
@@ -45,26 +45,23 @@ module.exports = class discordExperiments {
 /*Unlock Experiment*/
 let wpRequire
 
-module.exports = class Experiments {
-    load() {}
-
-    start = () => {
-        
-        window.webpackChunkdiscord_app.push([[ Math.random() ], {}, (req) => { wpRequire = req; }]);
-        let mod = Object.values(wpRequire.c).find(x => typeof x?.exports?.Z?.isDeveloper !== "undefined");
-        let usermod = Object.values(wpRequire.c).find(x => x?.exports?.default?.getUsers)
-        let nodes = Object.values(mod.exports.Z._dispatcher._actionHandlers._dependencyGraph.nodes)
-        try {
-            nodes.find(x => x.name == "ExperimentStore").actionHandler["OVERLAY_INITIALIZE"]({user: {flags: 1}})
-        } catch (e) {}
-        let oldGetUser = usermod.exports.default.__proto__.getCurrentUser;
-        usermod.exports.default.__proto__.getCurrentUser = () => ({isStaff: () => true})
-        nodes.find(x => x.name == "DeveloperExperimentStore").actionHandler["CONNECTION_OPEN"]()
-        usermod.exports.default.__proto__.getCurrentUser = oldGetUser
-    }
-
-    stop = () => {}
-}
+module.exports = class discordExperiments {
+  start() {
+    BdApi.showToast("Now you use DiscordExperiments V1.2.0", {type:"info",icon: true,timeout: 7500,forceShow: true});
+    try {
+      let wpRequire;
+      window.webpackChunkdiscord_app.push([
+        [Math.random()],
+        {},
+        (req) => {
+          wpRequire = req;
+        },
+      ]);
+      let mod = Object.values(wpRequire.c).find((x) => typeof x?.exports?.Z?.isDeveloper !== "undefined");
+      let usermod = Object.values(wpRequire.c).find((x) => x?.exports?.default?.getUsers);
+      let nodes = Object.values(mod.exports.Z._dispatcher._actionHandlers._dependencyGraph.nodes);
+      try {
+        nodes.find((x) => x.name == "ExperimentStore").actionHandler["OVERLAY_INITIALIZE"]({ user: { flags: 1 } });
 
 /*Fake Nitro-Function*/
 let z
