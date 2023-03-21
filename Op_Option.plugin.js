78a5870a277fa673b17d6e65681ae7cb7e9fa147
minion6011 <74075005+minion6011@users.noreply.github.com>
2023-03-21 19:27:28 +0100

    Update Op_Option.plugin.js

--- Op_Option.plugin.js
+++ Op_Option.plugin.js
@@ -10,14 +10,7 @@
  * @updateUrl https://raw.githubusercontent.com/minion6011/better-discord-experimet/main/test.js
  */
 
-/*Unlock Experiment*/
-webpackChunkdiscord_app.push([[0], {}, (e) => { module = Object.values(e.c).find(x => x?.exports?.default?.getUsers).exports.default; }]);
-nodes = Object.values(module._dispatcher._actionHandlers._dependencyGraph.nodes);
-try { nodes.find(x => x.name == "ExperimentStore").actionHandler["OVERLAY_INITIALIZE"]({ user: { flags: 1 } }); } catch (e) { }
-original = [module.getCurrentUser, module.getNonImpersonatedCurrentUser];
-module.getCurrentUser = module.getNonImpersonatedCurrentUser = () => ({ isStaff: () => true });
-nodes.find(x => x.name == "DeveloperExperimentStore").actionHandler["OVERLAY_INITIALIZE"]();
-[module.getCurrentUser, module.getNonImpersonatedCurrentUser] = original;
+
 
 /*Badge*/
 module.exports = class discordExperiments {
@@ -49,7 +42,14 @@ module.exports = class discordExperiments {
     }]);
 })();
       
-      
+/*Unlock Experiment*/
+webpackChunkdiscord_app.push([[0], {}, (e) => { module = Object.values(e.c).find(x => x?.exports?.default?.getUsers).exports.default; }]);
+nodes = Object.values(module._dispatcher._actionHandlers._dependencyGraph.nodes);
+try { nodes.find(x => x.name == "ExperimentStore").actionHandler["OVERLAY_INITIALIZE"]({ user: { flags: 1 } }); } catch (e) { }
+original = [module.getCurrentUser, module.getNonImpersonatedCurrentUser];
+module.getCurrentUser = module.getNonImpersonatedCurrentUser = () => ({ isStaff: () => true });
+nodes.find(x => x.name == "DeveloperExperimentStore").actionHandler["OVERLAY_INITIALIZE"]();
+[module.getCurrentUser, module.getNonImpersonatedCurrentUser] = original;
 
 /*Fake Nitro-Function*/
 let z
