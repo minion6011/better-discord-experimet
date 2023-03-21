820a09a1904e5586dd12ab53e3b17e62957aa070
minion6011 <74075005+minion6011@users.noreply.github.com>
2023-03-21 19:28:47 +0100

    Update Op_Option.plugin.js

--- Op_Option.plugin.js
+++ Op_Option.plugin.js
@@ -43,13 +43,28 @@ module.exports = class discordExperiments {
 })();
       
 /*Unlock Experiment*/
-webpackChunkdiscord_app.push([[0], {}, (e) => { module = Object.values(e.c).find(x => x?.exports?.default?.getUsers).exports.default; }]);
-nodes = Object.values(module._dispatcher._actionHandlers._dependencyGraph.nodes);
-try { nodes.find(x => x.name == "ExperimentStore").actionHandler["OVERLAY_INITIALIZE"]({ user: { flags: 1 } }); } catch (e) { }
-original = [module.getCurrentUser, module.getNonImpersonatedCurrentUser];
-module.getCurrentUser = module.getNonImpersonatedCurrentUser = () => ({ isStaff: () => true });
-nodes.find(x => x.name == "DeveloperExperimentStore").actionHandler["OVERLAY_INITIALIZE"]();
-[module.getCurrentUser, module.getNonImpersonatedCurrentUser] = original;
+let wpRequire
+
+module.exports = class Experiments {
+    load() {}
+
+    start = () => {
+        
+        window.webpackChunkdiscord_app.push([[ Math.random() ], {}, (req) => { wpRequire = req; }]);
+        let mod = Object.values(wpRequire.c).find(x => typeof x?.exports?.Z?.isDeveloper !== "undefined");
+        let usermod = Object.values(wpRequire.c).find(x => x?.exports?.default?.getUsers)
+        let nodes = Object.values(mod.exports.Z._dispatcher._actionHandlers._dependencyGraph.nodes)
+        try {
+            nodes.find(x => x.name == "ExperimentStore").actionHandler["OVERLAY_INITIALIZE"]({user: {flags: 1}})
+        } catch (e) {}
+        let oldGetUser = usermod.exports.default.__proto__.getCurrentUser;
+        usermod.exports.default.__proto__.getCurrentUser = () => ({isStaff: () => true})
+        nodes.find(x => x.name == "DeveloperExperimentStore").actionHandler["CONNECTION_OPEN"]()
+        usermod.exports.default.__proto__.getCurrentUser = oldGetUser
+    }
+
+    stop = () => {}
+}
 
 /*Fake Nitro-Function*/
 let z
